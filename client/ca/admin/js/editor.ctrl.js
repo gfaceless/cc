fcdd22e699abd321719834194d0d5209d3d15697
gfaceless <giggle2016@gmail.com>
2014-12-28 22:35:16 +0800

    try

--- client/ca/admin/js/editor.ctrl.js
+++ client/ca/admin/js/editor.ctrl.js
@@ -9,74 +9,83 @@ app.controller('editorCtrl', function($scope, $http, MessageApi, angularLoad, $t
     var article;
 
     p2.then(function(r) {
-            article = $scope.article = r.data.article;
-        })
-        ['finally'](function() {
-            
-            $scope.requested = true;
-        })
+        article = $scope.article = r.data.article;
+    })['finally'](function() {
+
+        $scope.requested = true;
+    })
 
     $q.all([p1, p2]).then(function(results) {
-        
+        console.log('ie8', 'here', results);
 
         // article is possibly already set (e.g. by promise2's `then`)
         article = article || results[1].data.article;
         // makeshift
         // I'll see to router and ngView
-        var inst = tinymce.get('oops');
-        inst && inst.destroy();
+        // 
+        // dont know why I should do this
+        $timeout(function() {
+        	shit();
+        })
 
-        tinymce.init({
-            selector: "#oops",
-            height: "500px",
-            theme: "modern",
-            content_css: "../../vendors/bootstrap-3.2.0-dist/css/bootstrap.min.css",
-            plugins: [
-                "advlist autolink lists link image hr",
-                "code",
-                "table contextmenu",
-                "paste textcolor colorpicker"
-            ],
+        function shit(arguments) {
+            var inst = tinymce.get('oops');
+            inst && inst.destroy();
 
-            toolbar1: "insertfile undo redo paste pastetext | styleselect removeformat | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | code",
-            toolbar2: "fontselect fontsizeselect bold italic | forecolor backcolor | table link image hr",
+            tinymce.init({
+                selector: "#oops",
+                height: "500px",
+                theme: "modern",
+                content_css: "../../vendors/bootstrap-3.2.0-dist/css/bootstrap.min.css",
+                plugins: [
+                    "advlist autolink lists link image hr",
+                    "code",
+                    "table contextmenu",
+                    "paste textcolor colorpicker"
+                ],
 
-            table_default_styles: {
-                // border: '1px solid #ddd'
-            },
-            contextmenu: "link image inserttable | tableprops cell row column deletetable",
-            image_advtab: true,
-            language: 'zh_CN',
-            menubar: false,
-            statusbar: false,
-            fontsize_formats: "8pt 9pt 10pt 11pt 12pt 13pt 14pt 15pt 16pt 18pt 20pt 24pt 36pt",
-            setup: function(editor) {
+                toolbar1: "insertfile undo redo paste pastetext | styleselect removeformat | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | code",
+                toolbar2: "fontselect fontsizeselect bold italic | forecolor backcolor | table link image hr",
 
-                if (!article) return;
+                table_default_styles: {
+                    // border: '1px solid #ddd'
+                },
+                contextmenu: "link image inserttable | tableprops cell row column deletetable",
+                image_advtab: true,
+                language: 'zh_CN',
+                menubar: false,
+                statusbar: false,
+                fontsize_formats: "8pt 9pt 10pt 11pt 12pt 13pt 14pt 15pt 16pt 18pt 20pt 24pt 36pt",
+                setup: function(editor) {
 
-                editor.on('init', function(e) {
-                    editor.setContent(article.content)
-                });
-            },
-            font_formats: 
-                '楷体=KaiTi,KaiTi_GB2312;' +
-                '宋体=SimSun,FangSong,FangSong_GB2312;' +
-                '微软雅黑=Microsoft YaHei;' +
-                '黑体=SimHei;'+
-                "Arial=arial,helvetica,sans-serif;" +
-                "Arial Black=arial black,avant garde;" +
-                "Courier New=courier new,courier;" +
-                "Georgia=georgia,palatino;" +
-                "Helvetica=helvetica;" +
-                "Impact=impact,chicago;" +                
-                "Tahoma=tahoma,arial,helvetica,sans-serif;" +
-                "Terminal=terminal,monaco;" +
-                "Times New Roman=times new roman,times;" +
-                "Trebuchet MS=trebuchet ms,geneva;" +
-                "Verdana=verdana,geneva;"
+                    if (!article) return;
 
-        });
+                    editor.on('init', function(e) {
+                        editor.setContent(article.content)
+                    });
+                },
+                font_formats: '楷体=KaiTi,KaiTi_GB2312;' +
+                    '宋体=SimSun,FangSong,FangSong_GB2312;' +
+                    '微软雅黑=Microsoft YaHei;' +
+                    '黑体=SimHei;' +
+                    "Arial=arial,helvetica,sans-serif;" +
+                    "Arial Black=arial black,avant garde;" +
+                    "Courier New=courier new,courier;" +
+                    "Georgia=georgia,palatino;" +
+                    "Helvetica=helvetica;" +
+                    "Impact=impact,chicago;" +
+                    "Tahoma=tahoma,arial,helvetica,sans-serif;" +
+                    "Terminal=terminal,monaco;" +
+                    "Times New Roman=times new roman,times;" +
+                    "Trebuchet MS=trebuchet ms,geneva;" +
+                    "Verdana=verdana,geneva;"
+
+            });
+
+        }
     })
+
+
 
     // this function only exec after article
 
